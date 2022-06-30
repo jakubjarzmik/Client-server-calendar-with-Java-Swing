cca87d0d73959c7073371760a10c9a320523df23
Jakub Jarzmik <jakubjarzmik00@gmail.com>
2022-06-30 19:12:20 +0200

    Remove duplicated directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Frames.Calendar"},{"l":"Frames.Calendar.Threads"},{"l":"Frames.Comparators"},{"l":"Frames.NewEvent"},{"l":"Server"},{"l":"StorageClasses"}];updateSearchResults();
